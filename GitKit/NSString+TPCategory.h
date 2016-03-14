05aed7461ae5d634c50fa8c7479261613b55d6fc
wanglanshou <wanglanshou@blinnnk.com>
2016-03-14 09:52:35 +0800

    first

---
+++ GitKit/NSString+TPCategory.h
@@ -0,0 +1,13 @@
+//
+//  NSString+TPCategory.h
+//  GitKit
+//
+//  Created by wang on 16/3/14.
+//  Copyright © 2016年 wp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (TPCategory)
++ (NSString *)UUID;
+@end
---
+++ GitKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitKit
+//
+//  Created by wang on 16/3/14.
+//  Copyright © 2016年 wp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
